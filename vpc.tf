4174c031cd4f5376965edb0bc8185f38c5d74f23
William Felipe <williamfelipe34@gmail.com>
2021-09-29 23:04:01 -0300

    first commit

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "new-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "subnets" {
+  count = 5
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id = aws_vpc.new-vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index}"
+  }
+}
